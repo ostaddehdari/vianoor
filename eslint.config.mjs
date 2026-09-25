fb2295730120eae3b7db5c547072da18ff46f2f8
ostaddehdari <ostaddehdari@gmail.com>
2026-09-26 00:18:01 +0330

    مرحله ۰۱: ایجاد ساختار مونوریپو و استانداردهای توسعه

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/.next/**', '**/node_modules/**', '**/next-env.d.ts'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
+  { files: ['**/*.tsx'], rules: { '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } },
+);
